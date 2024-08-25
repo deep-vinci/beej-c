0ce8aff356b3e2bd6092066df91413cb77230a95
Deepak <deepakworkpc@gmail.com>
2024-08-25 22:54:03 +0530

    docs: add gcc warnings

--- hello.c
+++ hello.c
@@ -4,4 +4,16 @@ int main(void) {
     printf("Hello, World!\n"); 
 }
 
-// C pre processor processes the #include to add the stdio.h before compilation
+/*
+C pre processor processes the #include to add the stdio.h before compilation
+
+#include
+#define
+
+gcc -Wall -Wextra -std=99 -pedantic foo.c
+-Wall generates warnings 
+-Wextra generates additional warnings that might be missed by -Wall
+-std=99 standard C99 features 
+-pedantic strictly adheres to the lang standard specified by -std
+
+*/
