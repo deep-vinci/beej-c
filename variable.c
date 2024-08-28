8b326d887a170d6ea31ac00b6cf64858f1ab930b
Deepak <deepakworkpc@gmail.com>
2024-08-28 16:10:09 +0530

    docs: variables are not containers, but a name for memeory address

--- variable.c
+++ variable.c
@@ -9,5 +9,6 @@ int main(void) {
 Variable - human readable name that refers to some data in memory
 
 Memory is a big array of bytes
-
+each bytes = 8 bits 0000 0000 
+each memory index can be refered to it by its index or address, or a loc or a pointer
 */
